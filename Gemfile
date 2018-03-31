faa74bfce6dc7d22f7489515c12e17dfcdf0c9eb
Jungho Bang <bjungho@caen-vnc-vm08.engin.umich.edu>
2018-03-31 03:18:50 -0400

    spotify api / mpd data parsing

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 gem 'ffi', '1.9.18'
+gem 'rspotify'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,12 @@
 class Playlist < ApplicationRecord
     has_many :includes, :foreign_key => "playlist_id", :dependent => :destroy
     has_many :tracks, :through => :includes, :source => :track
+
+    def self.read_file(index)
+	name = "public/mpd.slice.%d-%d.json" % [index, (index+1)*1000-1]
+	file = File.read(name)
+	data = JSON.parse(file)
+	return data
+    end
+
 end
