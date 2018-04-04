48e813a51f6659a418d4910c02b5b60b7735bc8f
Jungho Bang <me@bangtoven.com>
2018-04-03 22:23:21 -0400

    load artists

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,11 @@
 class Artist < ApplicationRecord
+  def self.load_hadoop_result
+    f = File.open("public/mpd_result/artist_output.txt", "r")
+    f.each_line do |line|
+      value = line.split("\t")[1]
+      json = JSON.parse(value)
+      json["name"] = json["name"][0..255]
+      Artist.create(json)
+    end
+  end
 end
